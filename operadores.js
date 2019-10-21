f07e09ef415332a27ae1f01e164944a978452d31
malumart <malu.martin92@hotmail.com>
2019-10-21 20:17:47 -0300

    Ejercicios del 27 al 32 operadores JS

---
+++ operadores.js
@@ -0,0 +1,50 @@
+// ejercicio 27
+
+let numero1;
+let numero2;
+
+numero1 = 15;
+numero2 = 10;
+
+console.log(`La suma de ${numero1} y ${numero2} es:`, numero1 + numero2);
+console.log(`La resta de ${numero1} y ${numero2} es:`, numero1 - numero2);
+console.log(`La multiplicacion de ${numero1} por ${numero2} es:`, numero1 * numero2);
+console.log(`La division de ${numero1} entre ${numero2} es:`, numero1 / numero2);
+console.log(`El resto de dividir ${numero1} por ${numero2} es:`, numero1 % numero2);
+
+
+// ejercicio 28
+
+let numero = 9;
+
+console.log(`La tabla de ${numero} es:`, numero * 1, numero * 2, numero * 3, numero * 4, numero * 5, numero * 6, numero * 7, numero * 8, numero * numero, numero * 10);
+
+
+// ejercicio 29
+
+let longitudLado = 10;
+let perimetro = longitudLado * 4;
+
+console.log(`El perimetro del cuadrado de longitud de lado ${longitudLado} es`, perimetro);
+
+// ejercicio 30
+longitudLado = 5;
+let area = longitudLado * longitudLado;
+console.log(`El area del cuadrado de longitud de lado ${longitudLado} es`, area);
+
+// ejercicio 31
+let lado1 = 10;
+let lado2 = 20;
+let lado3 = 5;
+
+console.log(`El perimetro del triangulo de lados ${lado1}, ${lado2}, ${lado3} es:`, lado1 + lado2 + lado3);
+
+// ejercicio 32
+
+let altura = 10;
+let base = 4;
+area = (altura * base) ;
+perimetro = altura * 2 + base * 2;
+console.log(`El perimetro del rectangulo es`, perimetro );
+console.log(`El area del rectangulo es`, area);
+
